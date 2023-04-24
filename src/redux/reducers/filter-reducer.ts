ca2448524ab2909734a7b8e9823a2a8e009f6a6f
Roman Chuchval <rchuchval@gmail.com>
2023-04-24 23:30:28 +0300

    refactor filter state for tasksLists and tasks

--- src/redux/reducers/filter-reducer.ts
+++ src/redux/reducers/filter-reducer.ts
@@ -1,11 +1,15 @@
-
 //========== TYPES ========== //
 export type PriorityTypes = 'Low' | 'Medium' | 'High'
 
 export type InitFilterStateType = {
+    tasksLists: FiltersGroupType
+    tasks: FiltersGroupType
+}
+export type FiltersGroupType = {
     priorityFilter: Array<PriorityTypes>
     isDoneFilter: boolean | null
 }
+export type FilteringEntityType = 'tasksLists' | 'tasks'
 
 type ActionsTypes = ReturnType<typeof changePriorityFilterAC>
     | ReturnType<typeof changeStatusFilterAC>
@@ -15,23 +19,35 @@ type ActionsTypes = ReturnType<typeof changePriorityFilterAC>
 //========== REDUCES ========== //
 
 const initFilterState: InitFilterStateType = {
-    priorityFilter: [],
-    isDoneFilter: null
+    tasksLists: {
+        priorityFilter: [],
+        isDoneFilter: null
+    },
+    tasks: {
+        priorityFilter: [],
+        isDoneFilter: null
+    }
 }
 
 export const filterReducer = (state: InitFilterStateType = initFilterState, action: ActionsTypes): InitFilterStateType => {
     switch (action.type) {
         case CHANGE_PRIORITY_FILTER:
-            return {...state, priorityFilter:
-                    state.priorityFilter.includes(action.payload.newFilterValue)
-                    ? state.priorityFilter.filter(f => f !== action.payload.newFilterValue)
-                    : [...state.priorityFilter, action.payload.newFilterValue]
-                    }
+            return {
+                ...state, [action.payload.filteringEntity]:
+                    {...state[action.payload.filteringEntity], priorityFilter:
+                    state[action.payload.filteringEntity].priorityFilter.includes(action.payload.newFilterValue)
+                        ? state[action.payload.filteringEntity].priorityFilter
+                            .filter(f => f !== action.payload.newFilterValue)
+                        : [...state[action.payload.filteringEntity].priorityFilter, action.payload.newFilterValue]
+            }}
         case CHANGE_STATUS_FILTER:
-            return {...state, isDoneFilter: action.payload.newFilterValue}
+            return {...state, [action.payload.filteringEntity]:
+                    {...state[action.payload.filteringEntity],isDoneFilter: action.payload.newFilterValue}}
         case RESET_ALL_FILTERS:
-            return {...state, priorityFilter: [], isDoneFilter: null}
-        default: return state
+            return {...state, [action.payload.filteringEntity]:
+                    {...state[action.payload.filteringEntity], priorityFilter: [], isDoneFilter: null}}
+        default:
+            return state
     }
 }
 
@@ -41,26 +57,29 @@ const CHANGE_PRIORITY_FILTER = 'CHANGE_PRIORITY_FILTER'
 const CHANGE_STATUS_FILTER = 'CHANGE_STATUS_FILTER'
 const RESET_ALL_FILTERS = 'RESET_ALL_FILTERS'
 
-export const changePriorityFilterAC = (newFilterValue: PriorityTypes) => {
+export const changePriorityFilterAC = (filteringEntity: FilteringEntityType, newFilterValue: PriorityTypes) => {
     return {
         type: CHANGE_PRIORITY_FILTER,
         payload: {
-            newFilterValue
+            newFilterValue,
+            filteringEntity
         }
     } as const
 }
 
-export const changeStatusFilterAC = (newFilterValue: boolean | null) => {
+export const changeStatusFilterAC = (filteringEntity: FilteringEntityType, newFilterValue: boolean | null) => {
     return {
         type: CHANGE_STATUS_FILTER,
         payload: {
-            newFilterValue
+            newFilterValue,
+            filteringEntity
         }
     } as const
 }
 
-export const resetFiltersAC = () => {
+export const resetFiltersAC = (filteringEntity: FilteringEntityType) => {
     return {
-        type: RESET_ALL_FILTERS
+        type: RESET_ALL_FILTERS,
+        payload: {filteringEntity}
     } as const
 }
